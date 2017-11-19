622cec4db4edddcf7b61b6d1b2cad0f1584e0543
OreoSammich <richardmillar12@gmail.com>
2017-11-18 22:20:49 -0800

    Add Vehicle implementation file

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A repository for the CPE400 Networking Programming Project
 Each vehicle on the network is an abstraction of a network packet. This allows each one to hold and share only basic information, such as identification, source, and destination addresses. The majority of routing of these packets is therefore completed by the intersection routers. The amount of time spend between each node is recorded by the packet, which is then read by the router and passed to the central device. This information helps the Central Node determine wait times at each individual node; this in turn allows it to scale and reroute packets accordingly.
 
 
-Class Device:
+Class Vehicle:
 
 	* Methods:
 
--- Vehicle.h
+++ Vehicle.h
@@ -6,24 +6,24 @@
 
 class Vehicle 
 {
-public:
-	Vehicle();
-    ~Vehicle();
+	public:
+		Vehicle();
+    	~Vehicle();
 
-	void setDepart(int time);
-	int getElapsedTime() const;
-	int getDestination() const;
+		void setDepart(int currTime);
+		int getElapsedTime() const;
+		int getDestination() const;
 
-    void requestRoute(const CentralComputeNode & ccn);
-    void setRoute(std::list<int> route);
+		void requestRoute(const CentralComputeNode & ccn);
+		void setRoute(std::list<int> route);
 
-private:
-	int id;
-	int sourceAddress;
-	int destAddress;
-	int departTime;
+	private:
+		int id;
+		int sourceAddress;
+		int destAddress;
+		int departTime;
 
-    std::list<int> route;
+		std::list<int> route;
 };
 
 #endif
