e6c80a4c5459318c6426b9c9e89c7a4c85700a14
OreoSammich <richardmillar12@gmail.com>
2017-11-18 22:06:49 -0800

    Change Device to Vehicle

---
+++ Vehicle.h
@@ -0,0 +1,29 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <list>
+#include "CentralComputeNode.h"
+
+class Vehicle 
+{
+public:
+	Vehicle();
+    ~Vehicle();
+
+	void setDepart(int time);
+	int getElapsedTime() const;
+	int getDestination() const;
+
+    void requestRoute(const CentralComputeNode & ccn);
+    void setRoute(std::list<int> route);
+
+private:
+	int id;
+	int sourceAddress;
+	int destAddress;
+	int departTime;
+
+    std::list<int> route;
+};
+
+#endif
